e6606f509f2fad5156f969abf937bf37edcdcd72
Dzmitry Marudau <vinmaster@tut.by>
2020-12-01 17:36:57 +0300

    Smart Calculator - Stage 4/8 Add substractions

--- Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/task/src/calculator/Main.java
@@ -2,15 +2,21 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
- * A program that reads two numbers in a loop and prints the sum in the standard output. If a user enters only a
- * single number, the program should print the same number. If a user enters an empty line, the program should ignore
- * it.
+ * A program that reads two numbers in a loop and calculates the result in the standard output.
+ * If a user enters only a single number, the program should print the same number.
+ * If a user enters an empty line, the program should ignore it.
  */
 public class Main {
+
+    private final static Pattern IS_DIGIT = Pattern.compile("[+-]?\\d+");
+
+    private static final Map<Character, Character> OPPOSITE_OPERATORS = Map.of('+', '+', '-', '+', '*', '*', '/', '/');
 
     private static final String COMMAND_EXIT = "/exit";
     private static final String MESSAGE_BYE = "Bye!";
@@ -33,21 +39,47 @@ public class Main {
     }
 
     private static void processInputNumbers(String input) {
-        List<Integer> inputNumbers = Arrays.stream(input.split("\\s+"))
-                                           .map(Integer::parseInt)
-                                           .collect(Collectors.toList());
+        List<String> inputNumbers = Arrays.stream(input.split("\\s+"))
+                                          .collect(Collectors.toList());
 
-        switch (inputNumbers.size()) {
-        case 1:
+        if (inputNumbers.size() == 1) {
             System.out.println(inputNumbers.get(0));
-            break;
-        default:
+        } else {
             System.out.println(process(inputNumbers));
-            break;
         }
     }
 
-    private static int process(List<Integer> numbers) {
-        return numbers.stream().reduce(0, Integer::sum);
+    private static int process(List<String> numbers) {
+        //return numbers.stream().reduce(0, Integer::sum);
+        int result = 0;
+        char operator = '+';
+        for (String number : numbers) {
+            if (IS_DIGIT.matcher(number).matches()) {
+                switch (operator) {
+                case '+':
+                    result += Integer.parseInt(number);
+                    break;
+                case '-':
+                    result -= Integer.parseInt(number);
+                    break;
+                default:
+                    break;
+                }
+
+            } else {
+                operator = identifyOperator(number);
+            }
+        }
+        return result;
+    }
+
+    private static char identifyOperator(String number) {
+        char operator;
+        String operators = number.trim();
+        char initialOperator = operators.charAt(0);
+        operator = operators.length() % 2 == 0
+                   ? OPPOSITE_OPERATORS.get(initialOperator)
+                   : initialOperator;
+        return operator;
     }
 }
