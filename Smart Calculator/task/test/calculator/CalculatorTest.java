333cf4180206fe82e2a4d85ba4f5398dd9e44a38
Dzmitry Marudau <vinmaster@tut.by>
2020-12-12 15:16:49 +0300

    Test existing calculate method

--- Smart Calculator/task/test/calculator/CalculatorTest.java
+++ Smart Calculator/task/test/calculator/CalculatorTest.java
@@ -17,6 +17,19 @@ public class CalculatorTest {
     private final Calculator calculator = new Calculator();
 
     @Test
+    public void calculate() {
+        Assert.assertEquals(8, calculator.calculate("5 + 3"));
+        Assert.assertEquals(12, calculator.calculate("5 + 4 + 3"));
+        Assert.assertEquals(14, calculator.calculate("5 + 4 + 3 + 2"));
+
+        Assert.assertEquals(2, calculator.calculate("5 - 3"));
+        Assert.assertEquals(1, calculator.calculate("5 - 3 - 1"));
+        Assert.assertEquals(2, calculator.calculate("5 - 1 - 1 - 1"));
+
+        Assert.assertEquals(6, calculator.calculate("5 + 1 -- 1 - 1"));
+    }
+
+    @Test
     public void infixToPostfix() {
         Assert.assertEquals("1 2 +", infixToPostfixAsString(List.of("1","+","2")));
         Assert.assertEquals("1 2 *", infixToPostfixAsString(List.of("1", "*", "2")));
