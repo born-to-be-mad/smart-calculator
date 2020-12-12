a28da45f3fb247c0313aeb81a1a3f32c23db0019
Dzmitry Marudau <vinmaster@tut.by>
2020-12-12 15:10:41 +0300

    Extend Calculator test

--- Smart Calculator/task/test/calculator/CalculatorTest.java
+++ Smart Calculator/task/test/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
@@ -17,15 +18,21 @@ public class CalculatorTest {
 
     @Test
     public void infixToPostfix() {
-
         Assert.assertEquals("1 2 +", infixToPostfixAsString(List.of("1","+","2")));
         Assert.assertEquals("1 2 *", infixToPostfixAsString(List.of("1", "*", "2")));
         Assert.assertEquals("1 2 * 3 +", infixToPostfixAsString(List.of("1", "*", "2", "+", "3")));
         Assert.assertEquals("1 2 * 3 +", infixToPostfixAsString(List.of("1", "*", "2", "+", "3")));
         Assert.assertEquals("1 2 * 3 4 * +", infixToPostfixAsString(List.of("1", "*", "2", "+", "3", "*", "4")));
+
+        Assert.assertEquals("3 2 4 * +", infixToPostfixAsString("3 + 2 * 4"));
+        Assert.assertEquals("2 3 4 + * 1 +", infixToPostfixAsString("2 * ( 3 + 4 ) + 1"));
     }
 
     private String infixToPostfixAsString(List<String> strings) {
         return String.join(" ", calculator.infixToPostfix(strings));
+    }
+
+    private String infixToPostfixAsString(String string) {
+        return String.join(" ", calculator.infixToPostfix(Arrays.asList(string.split("\\s+"))));
     }
 }
